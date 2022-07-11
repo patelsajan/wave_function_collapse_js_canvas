453e44e27e3590e5bd1a0283f8f7a671f87cc86a
patelsajan <patelsajan86@gmail.com>
2022-07-11 15:07:56 +0530

    some refactoring

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-var canvas = document.querySelector("canvas");
-
-let window_height = window.innerHeight;
-let window_width = window.innerWidth;
-
-// set dimenstions of the canvas
-canvas.height = window_height;
-canvas.width = window_width;
-// debugger;
+let canvas;
+let window_height;
+let window_width;
 let cell_size = 50;
-let num_horizontal_cells = Math.floor(window_width / cell_size) - 1;
-let num_vertical_cells = Math.floor(window_height / cell_size) - 1;
-
-var rendering_context = canvas.getContext("2d");
-console.log("rendering_context:", rendering_context);
-
-rendering_context.fillStyle = "#ff0000";
-rendering_context.strokeStyle = "black";
+let num_horizontal_cells;
+let num_vertical_cells;
+let rendering_context;
 
 const tiles = {
   0: {
@@ -39,49 +28,6 @@ const tiles = {
     edges: [1, 0, 1, 1],
   },
 };
-let grid = [];
-for (let i = 0; i <= num_vertical_cells; i++) {
-  for (let j = 0; j <= num_horizontal_cells; j++) {
-    if (grid[i] == undefined) {
-      grid[i] = [];
-    }
-    // rendering_context.strokeRect(j*cell_size, i*cell_size, cell_size, cell_size);
-    let required_top_left_edge = {
-      top: -1,
-      left: -1,
-    };
-    // check bottom edge of the cell on top of the current one.
-    if (!(i - 1 < 0)) {
-      required_top_left_edge.top = grid[i - 1][j].tile.edges[2];
-    }
-    if (!(j - 1 < 0)) {
-      required_top_left_edge.left = grid[i][j - 1].tile.edges[1];
-    }
-    let tile = tiles[getTile(required_top_left_edge)];
-
-    let image = new Image(cell_size, cell_size);
-    image.src = tile.image_src;
-    rendering_context.drawImage(
-      image,
-      j * cell_size,
-      i * cell_size,
-      cell_size,
-      cell_size
-    );
-
-    grid[i][j] = {
-      i,
-      j,
-      loc_x: j * cell_size,
-      loc_y: i * cell_size,
-      cell_size,
-      required_top_left_edge,
-      tile,
-    };
-  }
-}
-
-console.log(grid);
 
 function getTile(required_top_left_edge) {
   let possibleTiles = [];
@@ -108,3 +54,82 @@ function getTile(required_top_left_edge) {
     return 0;
   }
 }
+
+let grid = [];
+
+function draw() {
+  let flat_grid = grid.flat();
+
+  flat_grid.forEach((cell) => {
+    rendering_context.drawImage(
+      cell.image,
+      cell.loc_x,
+      cell.loc_y,
+      cell_size,
+      cell_size
+    );
+  });
+}
+
+function evaluateCells() {
+  for (let i = 0; i <= num_vertical_cells; i++) {
+    for (let j = 0; j <= num_horizontal_cells; j++) {
+      if (grid[i] == undefined) {
+        grid[i] = [];
+      }
+      // rendering_context.strokeRect(j*cell_size, i*cell_size, cell_size, cell_size);
+      let required_top_left_edge = {
+        top: -1,
+        left: -1,
+      };
+      // check bottom edge of the cell on top of the current one.
+      if (!(i - 1 < 0)) {
+        required_top_left_edge.top = grid[i - 1][j].tile.edges[2];
+      }
+      // check right edge of the cell to the left.
+      if (!(j - 1 < 0)) {
+        required_top_left_edge.left = grid[i][j - 1].tile.edges[1];
+      }
+      let tile = tiles[getTile(required_top_left_edge)];
+
+      let image = new Image(cell_size, cell_size);
+      image.src = tile.image_src;
+
+      grid[i][j] = {
+        i,
+        j,
+        loc_x: j * cell_size,
+        loc_y: i * cell_size,
+        cell_size,
+        required_top_left_edge,
+        tile,
+        image,
+      };
+    }
+  }
+
+  console.log(grid);
+  draw();
+}
+
+function prepareCanvas() {
+  canvas = document.querySelector("canvas");
+
+  window_height = window.innerHeight;
+  window_width = window.innerWidth;
+
+  // set dimenstions of the canvas
+  canvas.height = window_height;
+  canvas.width = window_width;
+  num_horizontal_cells = Math.floor(window_width / cell_size) - 1;
+  num_vertical_cells = Math.floor(window_height / cell_size) - 1;
+
+  rendering_context = canvas.getContext("2d");
+  console.log("rendering_context:", rendering_context);
+
+  rendering_context.fillStyle = "#ff0000";
+  rendering_context.strokeStyle = "black";
+  evaluateCells();
+}
+
+window.addEventListener("load", (_) => prepareCanvas());
